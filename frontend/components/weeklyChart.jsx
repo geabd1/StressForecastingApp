b5de8017bf00419dd332cc0573afc00f65dbcb3b
G. Abdalla <geabd1@morgan.edu>
2025-11-04 21:45:21 -0500

    removed unnessary files

--- frontend/components/weeklyChart.jsx
+++
@@ -1,4 +0,0 @@
-<div class="weekly-chart-container">
-  <h2>Weekly Mood Overview</h2>
-  <canvas id="moodChart"></canvas>
-</div>
--- frontend/pages/signUp.php
+++
@@ -1,4 +0,0 @@
-<?php
-session_start();
-include '../../database/db.php';
-?>
